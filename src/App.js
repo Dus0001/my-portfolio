24cffc71ff270d0481c9cb117b71c90e221b64ea
Daven <davensteen@gmail.com>
2023-01-16 16:10:45 -0600

    create projects and photolist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
+  const  [categories] = useState([
+    { name: 'portfolio', description: 'Links to my projects' },
+    { name: 'resume', description: 'A link to my resume' },
+
+  ]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
     <div>
-    <Nav></Nav>
+    <Nav>
+      categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}</Nav>
     <main>
+      <Projects currentCategory={currentCategory}>
+      </Projects>
       <About></About>
     </main>
   </div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,49 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-    const  categories = [
-        { name: 'portfolio', description: 'Links to my projects' },
-        { name: 'resume', description: 'A link to my resume' },
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+      } = props;
 
-      ];
-    
-      const handleClick = () => {
-        console.log("click handled")
-      }
-    
+      useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+      }, [currentCategory]);
+      
       return (
-        <header data-testid="header" className="flex-row px-1">
+        <header className="flex-row px-1">
           <h2>
-            <a href="/">
-              <span role="img" aria-label="camera"> 🎓</span> Odysnpm runsey
+            <a data-testid="link" href="/">
+              <span role="img" aria-label="camera">
+                {" "}
+                📸
+              </span>{" "}
+              Oh Snap!
             </a>
           </h2>
           <nav>
             <ul className="flex-row">
               <li className="mx-2">
-                <a href="#about" onClick={() => handleClick()}>
+                <a
+                  href="#about"
+                >
                   About me
                 </a>
               </li>
-              <li className={"mx-2"}>
-                <span onClick={() => handleClick()}>
-                  Contact
-                </span>
+              <li>
+                <span>Contact</span>
               </li>
-              {
-                categories.map((category) => (
-                  <li className="mx-1" key={category.name} >
-                    <span onClick={() => { handleClick(); }}>
-                     {capitalizeFirstLetter(category.name)}
-                    </span>
-                  </li>
-                ))
-              }
+              {categories.map((category) => (
+                <li className={`mx-1 ${
+                    currentCategory.name === category.name && 'navActive'
+                    }`} key={category.name}>
+                  <span
+                    onClick={() => {
+                      setCurrentCategory(category)
+                    }}
+                  >
+                    {capitalizeFirstLetter(category.name)}
+                  </span>
+                </li>
+              ))}
             </ul>
           </nav>
         </header>
-  );
+      );
 }
 
 export default Nav;
