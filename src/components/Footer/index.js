cad12d4f2256aad6406b6602cbde27242359c889
Daven <davensteen@gmail.com>
2023-01-17 04:44:12 -0600

    add footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+        <P>
+        Thank you for visiting! 
+        -Daven
+        </P>
+
+    </footer>
+    )
+};
